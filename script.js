88e2414937f8e7a5c064c83f2249ad34dedae70c
thalysonlima820 <thalysonlima820@gmail.com>
2023-11-18 12:48:11 -0300

    versao 2.0

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
 $(document).ready(() => {
 	
+    $('#documentacao').on('click', () => {
+
+        // METODO LOAD
+        //$("#pagina").load('documentacao.html')
+
+        // METODO GET
+        $.get('documentacao.html', data => {
+            $('#pagina').html(data)
+        })
+
+        // METODO POST
+        // $.post('documentacao.html', data => {
+        //      $('#pagina').html(data)
+        // })
+
+
+    })
+    $('#suporte').on('click', () => {
+        $.get('suporte.html', data => {
+            $('#pagina').html(data)
+        })
+    })
+
+
+    $('#competencia').on('change', e => {
+        // console.log($(e.target).val())
+
+        let competencia = $(e.target).val()
+        // console.log(competencia)
+
+        $.ajax({
+            type: 'GET',
+            url: 'app.php',
+            data: `competencia=${competencia}`,
+            dataType: 'json',
+            success: dados => { console.log(dados.numeroVendas, dados.totalVendas);
+                $('#numero_vendas').html(dados.numeroVendas);
+                $('#total_vendas').html( 'R$ ' + dados.totalVendas)
+            },
+            error: erro => { console.log(erro)}
+        })
+    })
+
+
+
 })
